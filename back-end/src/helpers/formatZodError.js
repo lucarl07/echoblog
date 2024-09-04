de93659a1d95650b3bc5cf08f606d5baef6ed48b
lucarl07 <junitinho2007@gmail.com>
2024-09-04 10:16:53 -0300

    ✨ feat: New helper "formatZodError"

---
+++ back-end/src/helpers/formatZodError.js
@@ -0,0 +1,16 @@
+function formatZodError(error) {
+  const formattedError = {}
+
+  error.issues.forEach((err) => {
+    const path = err.path[0]
+
+    if (!formattedError[path]) {
+      formattedError[path] = []
+    }
+    formattedError[path].push(err.message)
+  })
+
+  return formattedError
+}
+
+export default formatZodError
