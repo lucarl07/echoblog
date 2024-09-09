aa3780323750b05fa2fe35cec13b95c30b1ac450
lucarl07 <junitinho2007@gmail.com>
2024-09-09 11:44:20 -0300

    🔒 Add Bearer Token Auth at endpoint PUT /usuarios/:id

--- back-end/src/routes/usuarioRoutes.js
+++ back-end/src/routes/usuarioRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 
 import { alterUserPermissions, getUserList, loginAsUser, registerUser, removeUserAccount, updateUser } from "../controller/usuariosController.js"
+import verifyUserToken from "../helpers/verifyUserToken.js";
 
 const router = Router();
 
 // Public Routes:
 router.post("/registro", registerUser)
 router.post("/login", loginAsUser)
-router.put("/:id", updateUser)
+router.put("/:id", verifyUserToken, updateUser)
 
 // Limited Routes (Admin-only):
 router.get("/", getUserList)
