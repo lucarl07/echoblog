ac71dfab4eff6153864e7a49fd6b5db5523a293a
lucarl07 <junitinho2007@gmail.com>
2024-09-09 08:54:23 -0300

    ✨ feat: New endpoint POST /usuarios/registro

--- back-end/src/controller/usuariosController.js
+++ back-end/src/controller/usuariosController.js
@@ -2,10 +2,37 @@
 import Usuario from "../model/usuarioModel.js";
 
 // Model Validations:
+import registerUserSchema from "../validators/registerUserSchema.js";
 
 // Helpers:
+import formatZodError from "../helpers/formatZodError.js";
 
-export const registerUser = async (req, res) => {}
+export const registerUser = async (req, res) => {
+  const bodyVal = registerUserSchema.safeParse(req.body)
+
+  if (!bodyVal.success) {
+    return res.status(400).json({
+      message: "Os dados recebidos do corpo da requisição são inválidos.",
+      details: formatZodError(bodyVal.error),
+    });
+  }
+
+  const { nome, email, senha, papel } = req.body
+
+  const newUser = { nome, email, senha, papel }
+
+  try {
+    await Usuario.create(newUser)
+    res.status(200).json({
+      message: "Usuário cadastrado com sucesso!"
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Erro interno durante o registro do usuário."
+    })
+  }
+}
 
 export const loginAsUser = async (req, res) => {}
 
