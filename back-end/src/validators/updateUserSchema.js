9c4f559208afefc426aa7295d242748cc32c9ee4
lucarl07 <junitinho2007@gmail.com>
2024-09-09 11:25:31 -0300

    🗄️ model: New validation schema for Usuario updates

---
+++ back-end/src/validators/updateUserSchema.js
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+
+const updateUserSchema = z.object({
+  nome: z.optional(
+    z.string()
+    .max(255, "O número de caracteres é maior que 255")
+  ),
+  email: z.optional(
+    z.string()
+    .regex(emailRegex, "O e-mail não está no formato adequado.")
+  ),
+  senha: z.optional(
+    z.string()
+    .min(8, "A senha deve ter pelo menos 8 caracteres.")
+    .regex(passwordRegex, "A senha deve conter letras maiúsculas, minúsculas e números.")
+  ),
+  papel: z.optional(
+    z.enum(["administrador", "autor", "leitor"])
+  )
+})
+
+export default updateUserSchema;
