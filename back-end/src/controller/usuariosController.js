1b76bf24b764db132de0c4436a46b2bf75178d05
lucarl07 <junitinho2007@gmail.com>
2024-09-09 09:53:36 -0300

    ✨ feat: New endpoint POST /usuarios/login

--- back-end/src/controller/usuariosController.js
+++ back-end/src/controller/usuariosController.js
@@ -3,10 +3,13 @@ import Usuario from "../model/usuarioModel.js";
 
 // Model Validations:
 import registerUserSchema from "../validators/registerUserSchema.js";
+import loginSchema from "../validators/loginSchema.js";
 
 // Helpers:
 import formatZodError from "../helpers/formatZodError.js";
 import hashPassword from "../helpers/hashPassword.js";
+import checkPassword from "../helpers/checkPassword.js";
+import createUserToken from "../helpers/createUserToken.js";
 
 export const registerUser = async (req, res) => {
   const bodyVal = registerUserSchema.safeParse(req.body)
@@ -41,7 +44,46 @@ export const registerUser = async (req, res) => {
   }
 }
 
-export const loginAsUser = async (req, res) => {}
+export const loginAsUser = async (req, res) => {
+  const bodyVal = loginSchema.safeParse(req.body)
+
+  if (!bodyVal.success) {
+    return res.status(400).json({
+      message: "Os dados recebidos do corpo da requisição são inválidos.",
+      details: formatZodError(bodyVal.error),
+    });
+  }
+
+  const { email, senha } = req.body
+
+  try {
+    const user = await Usuario.findOne({ where: {email} })
+
+    if (!user) {
+      return res.status(409).json({
+        message: "O e-mail não existe."
+      })
+    }
+    if (!checkPassword(senha, user.senha)) {
+      return res.status(409).json({
+        message: "A senha está incorreta."
+      })
+    }
+
+    const token = createUserToken(user)
+
+    res.status(200).json({
+      message: "Usuário logado com sucesso!",
+      id: user.usuario_id,
+      token
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Erro interno durante o login do usuário."
+    })
+  }
+}
 
 export const updateUser = async (req, res) => {}
 
