5322b5ab7568e9b25040de6321ff4436b3834400
lucarl07 <junitinho2007@gmail.com>
2024-09-11 09:58:58 -0300

    ✨ feat: New helper verifyAdminToken.js

---
+++ back-end/src/helpers/verifyAdminToken.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+
+const verifyAdminToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      err: "Acesso negado ao sistema."
+    })
+  }
+  
+  const token = req.headers.authorization.split(" ")[1]
+  
+  if (!token) {
+    return res.status(401).json({
+      err: "Acesso negado ao sistema."
+    })
+  }
+
+  try {
+    const tokenBearer = jwt.verify(token, process.env.USER_TOKEN_SECRET)
+    
+    if (tokenBearer.permissoes == "administrador") {
+      next()
+    } else {
+      res.status(401).json({
+        message: "O usuário não possui permissão para realizar a ação.",
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "O token fornecido é inválido.",
+      err: error
+    })
+  }
+}
+
+export default verifyAdminToken;
