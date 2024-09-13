14e7b1b9f78359057d2f09a38ea6888cb567f2d3
lucarl07 <junitinho2007@gmail.com>
2024-09-13 08:27:50 -0300

    🗄️ model: Update Usuario validator with new attrs and regex

--- back-end/src/validators/registerUserSchema.js
+++ back-end/src/validators/registerUserSchema.js
@@ -1,16 +1,18 @@
 import { z } from "zod";
 
-const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
 
 const registerUserSchema = z.object({
   nome: z.string()
     .max(255, "O número de caracteres é maior que 255"),
   email: z.string()
-    .regex(emailRegex, "O e-mail não está no formato adequado."),
+    .email("O e-mail não está no formato adequado."),
   senha: z.string()
     .min(8, "A senha deve ter pelo menos 8 caracteres.")
     .regex(passwordRegex, "A senha deve conter letras maiúsculas, minúsculas e números."),
+  imagem: z.optional(
+    z.string()
+  ),
   papel: z.optional(
     z.enum(["administrador", "autor", "leitor"])
   )
