64cdba9b3f3c2e1f6ca98a6f656173b5c0933608
lucarl07 <junitinho2007@gmail.com>
2024-10-04 08:44:20 -0300

    🐛 fix: Untreated errors at findTokenBearer helper

--- back-end/src/helpers/findTokenBearer.js
+++ back-end/src/helpers/findTokenBearer.js
@@ -2,9 +2,14 @@ import jwt from "jsonwebtoken"
 
 const findTokenBearer = (request) => {
   let auth = request.headers.authorization
-  let token = auth.split(" ")[1]
-  const tokenBearer = jwt.verify(token, process.env.USER_TOKEN_SECRET)
-  return tokenBearer;
+  
+  try {
+    let token = auth.split(" ")[1]
+    const tokenBearer = jwt.verify(token, process.env.USER_TOKEN_SECRET)
+    return tokenBearer;
+  } catch (error) {
+    return new Error(`Token inexistente (${error})`)
+  }
 }
 
 export default findTokenBearer;
